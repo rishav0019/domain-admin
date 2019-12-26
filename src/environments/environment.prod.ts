297acabd424d2dc20941a8a52d29880e6c79e465
devgrv <dgourav1992@gmail.com>
2019-12-26 12:44:55 +0530

    Setup Project for hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDtArLj5P865s-SrY0RuV47hzCMS-Ynorw",
-    authDomain: "domain-admin-a72a2.firebaseapp.com",
-    databaseURL: "https://domain-admin-a72a2.firebaseio.com",
-    projectId: "domain-admin-a72a2",
-    storageBucket: "domain-admin-a72a2.appspot.com",
-    messagingSenderId: "557629641918",
-    appId: "1:557629641918:web:c98d61fd26802726007aef",
-    measurementId: "G-QVLMWWTP40"
+    apiKey: "AIzaSyDuYlZNy7FCT6BNt6dErGWMvh3KUf2RCe4",
+    authDomain: "domain-project-51bd1.firebaseapp.com",
+    databaseURL: "https://domain-project-51bd1.firebaseio.com",
+    projectId: "domain-project-51bd1",
+    storageBucket: "domain-project-51bd1.appspot.com",
+    messagingSenderId: "853755565473",
+    appId: "1:853755565473:web:3acec26fbd32605ca1a7e0",
+    measurementId: "G-M9KHYYHTYH"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDtArLj5P865s-SrY0RuV47hzCMS-Ynorw",
-    authDomain: "domain-admin-a72a2.firebaseapp.com",
-    databaseURL: "https://domain-admin-a72a2.firebaseio.com",
-    projectId: "domain-admin-a72a2",
-    storageBucket: "domain-admin-a72a2.appspot.com",
-    messagingSenderId: "557629641918",
-    appId: "1:557629641918:web:c98d61fd26802726007aef",
-    measurementId: "G-QVLMWWTP40"
+    apiKey: "AIzaSyDuYlZNy7FCT6BNt6dErGWMvh3KUf2RCe4",
+    authDomain: "domain-project-51bd1.firebaseapp.com",
+    databaseURL: "https://domain-project-51bd1.firebaseio.com",
+    projectId: "domain-project-51bd1",
+    storageBucket: "domain-project-51bd1.appspot.com",
+    messagingSenderId: "853755565473",
+    appId: "1:853755565473:web:3acec26fbd32605ca1a7e0",
+    measurementId: "G-M9KHYYHTYH"
   },
-  
-};
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+};
